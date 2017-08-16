8b4e4c816704eca333478f371d987406a8fa1ffb
Lowie Benoot <lowie@teamleader.be>
2017-08-16 15:02:07 +0200

    added/moved some of the button examples

--- spec/components/button.js
+++ spec/components/button.js
@@ -11,7 +11,7 @@ const ButtonTest = () => (
 
     <h3>Different button types</h3>
     <p>
-      <Button primary>Primary</Button>&nbsp;
+      <Button primary onClick={handleItemClick}>Primary</Button>&nbsp;
       <Button secondary>Secondary</Button>&nbsp;
       <Button secondary bordered>Secondary bordered</Button>
     </p>
@@ -23,25 +23,28 @@ const ButtonTest = () => (
       <Button primary large>Large</Button>
     </p>
     <p>
-      <Button primary icon="search">Small</Button>&nbsp;
-      <Button primary medium icon="search">Medium</Button>&nbsp;
-      <Button primary large icon="search">Large</Button>
+      <Button secondary>Small</Button>&nbsp;
+      <Button secondary medium>Medium</Button>&nbsp;
+      <Button secondary large>Large</Button>
     </p>
     <p>
       <Button secondary bordered>Small</Button>&nbsp;
       <Button secondary medium bordered>Medium</Button>&nbsp;
       <Button secondary large bordered>Large</Button>
     </p>
+
+    <h3>Icons</h3>
+    <p>
+      <Button primary icon="search">Primary</Button>&nbsp;
+      <Button secondary icon="search">Secondary with icon</Button>&nbsp;
+      <Button secondary bordered icon="search">Secondary bordered with icon</Button>
+    </p>
+
     <h3>Icon only</h3>
     <p>
       <Button primary icon="search" />&nbsp;
       <Button primary medium icon="search" />&nbsp;
       <Button primary large icon="search" />
-    </p>
-    <p>
-      <Button primary onMouseUp={handleItemClick}>Primary</Button>&nbsp;
-      <Button primary icon="search">Primary with icon</Button>&nbsp;
-      <Button primary icon="search" />
     </p>
 
     <h3>Processing</h3>
@@ -57,45 +60,30 @@ const ButtonTest = () => (
       <Button primary disabled icon="search">Primary disabled with icon</Button>&nbsp;
       <Button primary disabled icon="search" />
     </p>
-
-    <hr />
-
-    <p>
-      <Button>Secondary</Button>&nbsp;
-      <Button icon="search">Secondary with icon</Button>&nbsp;
-      <Button icon="search" />
-    </p>
-
-    <p>
-      <Button bordered>Secondary bordered</Button>&nbsp;
-      <Button bordered icon="search">Secondary with icon and border</Button>&nbsp;
-    </p>
-
-    <p>
-      <Button processing>Secondary processing</Button>&nbsp;
-      <Button processing icon="search">Secondary processing with icon</Button>&nbsp;
-      <Button processing />
-    </p>
-
-    <p>
-      <Button bordered processing>Secondary bordered processing</Button>&nbsp;
-      <Button bordered processing icon="search">Secondary processing with icon</Button>&nbsp;
-      <Button bordered processing />
-    </p>
-
     <p>
       <Button disabled>Secondary disabled</Button>&nbsp;
       <Button disabled icon="search">Secondary disabled with icon</Button>&nbsp;
       <Button disabled icon="search" />
     </p>
-
     <p>
       <Button bordered disabled>Secondary bordered disabled</Button>&nbsp;
       <Button bordered disabled icon="search">Secondary disabled with icon</Button>&nbsp;
       <Button bordered disabled icon="search" />
     </p>
 
-    <hr />
+    <h3>Processing</h3>
+    <p>
+      <Button processing>Secondary processing</Button>&nbsp;
+      <Button processing icon="search">Secondary processing with icon</Button>&nbsp;
+      <Button processing />
+    </p>
+    <p>
+      <Button bordered processing>Secondary bordered processing</Button>&nbsp;
+      <Button bordered processing icon="search">Secondary processing with icon</Button>&nbsp;
+      <Button bordered processing />
+    </p>
+
+    <h3>Icon button</h3>
 
     <p><IconButton icon="search" /></p>
   </section>
